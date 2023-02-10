74c4fbffcfc8e7e300d18a3edc774de19034cb51
ScottMartino <scott.martino@gmail.com>
2023-02-10 11:37:31 -0500

    Initial API with params to get need info into variable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import './App.scss';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  
+  const userInput = "package"
+  
+  
+  
+  useEffect(()=>{
+
+    axios({
+      url: 'http://api.datamuse.com/words',
+      method: 'GET',
+      params:{
+        // ml : "construction",
+        // rel_bga: "construction",
+        md: 's',
+        sp: userInput,
+        max: 1
+      }
+    }).then((response)=>{
+      console.log(response.data)
+    })
+  },[])
+    
+  
+  
   return (
     <div className="App">
 
